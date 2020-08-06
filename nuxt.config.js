63a5caaa941b4ca631ce5242daef38e6f3e3bfc0
raphaelnoguier <raphael.aitelalim@gmail.com>
2020-08-06 23:18:01 +0200

    [v2] disable autofocus on input

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,8 @@ module.exports = {
 		title: 'Siena',
 		meta: [
 			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1"' },
 			{ hid: 'description', name: 'description', content: 'Here goes the site description' }
-		],
-		link: [
-			// { rel: 'icon', type: 'image/png', href: '/favicon.png' }
 		]
 	},
 	loading: false,
